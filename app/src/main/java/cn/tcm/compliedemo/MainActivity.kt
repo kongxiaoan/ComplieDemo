92ebf71d2c098dc76c03607811e930e9972d0d83
kongxiaoan <18153976553@163.com>
2017-12-13 18:25:03 +0800

    自定义Complie的依赖 测试

---
+++ app/src/main/java/cn/tcm/compliedemo/MainActivity.kt
@@ -0,0 +1,16 @@
+package cn.tcm.compliedemo
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+	override fun onCreate(savedInstanceState: Bundle?) {
+		super.onCreate(savedInstanceState)
+		initView()
+	}
+
+	private fun initView() {
+
+
+	}
+}
---
+++ mylibrary/src/main/java/cn/tcm/mylibrary/Test.kt
@@ -0,0 +1,12 @@
+package cn.tcm.mylibrary
+
+/**
+ * Created by mr.kong on 2017/12/13.
+ * 测试类
+ */
+class Test {
+
+	fun addTion(addnum1: Double, addnum2: Double): Double {
+		return addnum1.plus(addnum2)
+	}
+}
